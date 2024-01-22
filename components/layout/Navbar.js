35da79da0ac9f12bb129632a53a0c37ba785cb1f
Jacklyn Biggin <hi@jacklyn.dev>
2024-01-21 22:42:04 -0500

    update socials + blog link

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -20,7 +20,7 @@ export default function Navbar() {
           <Row>
               <Col xs={12} md={2} lg={1}><Link href="/"><span>🏠 <span className="d-inline d-md-none">Home</span></span></Link></Col>
               <Col xs={12} md><Link href="/projects"><span>Projects</span></Link></Col>
-              <Col xs={12} md><Link href="/blog"><span>Blog</span></Link></Col>
+              <Col xs={12} md><Link href="https://blog.jacklyn.dev"><span>Blog</span></Link></Col>
               <Col xs={12} md><Link href="/about"><span>About</span></Link></Col>
               <Col xs={12} md><Link href="/contact"><span>Contact</span></Link></Col>
           </Row>
--- components/layout/Socials.js
+++ components/layout/Socials.js
@@ -6,12 +6,11 @@ export default function Socials() {
     return (
         <div className={styles.iconcontainer}>
             <div className={styles.icons}>
-                <a href="https://linkedin.com/in/JackBiggin"><FontAwesomeIcon icon={faLinkedin} /></a>
+                <a href="https://linkedin.com/in/JacklynBiggin"><FontAwesomeIcon icon={faLinkedin} /></a>
                 <a href="https://twitter.com/JackBiggin"><FontAwesomeIcon icon={faTwitter} /></a>
-                <a href="https://youtube.com/JackBiggin"><FontAwesomeIcon icon={faYoutube} /></a>
+                <a href="https://youtube.com/@JacklynBiggin"><FontAwesomeIcon icon={faYoutube} /></a>
                 <a href="https://twitch.tv/JacklynBiggin"><FontAwesomeIcon icon={faTwitch} /></a>
                 <br className="d-none d-md-inline" />
-                <a className={styles.tag} href="https://timeline.jacklyn.dev"><div>Timeline</div></a>
                 <a className={styles.tag} href="./Jacklyn%20Biggin%20Resume.pdf"><div>Resume</div></a>
             </div>
         </div>
