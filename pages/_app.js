40a7d79a2a07c276b29b2eb43c5b7e549c9982c7
Jacklyn Biggin <hi@jacklyn.dev>
2024-01-23 23:44:18 -0500

    Fix font flash on page load

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,11 @@ function MyApp({ Component, pageProps }) {
 				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />*/}
 				<meta name="msapplication-TileColor" content="#45257e"></meta>
 				<meta name="theme-color" content="#45257e"></meta>
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+				<link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@400;700&family=Fredoka:wght@700&family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+
 			</Head>
 			<div className={styles.body}>
 				<Container>
