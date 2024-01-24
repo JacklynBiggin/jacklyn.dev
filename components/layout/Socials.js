e5866f929c7f234c76102166aedfc4d1c1dde244
Jacklyn Biggin <hi@jacklyn.dev>
2024-01-24 11:34:39 -0500

    update socials

--- components/layout/Socials.js
+++ components/layout/Socials.js
@@ -1,17 +1,19 @@
 import styles from './Socials.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faTwitter, faYoutube, faTwitch } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faTwitter, faYoutube, faTwitch, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function Socials() {
     return (
         <div className={styles.iconcontainer}>
             <div className={styles.icons}>
                 <a href="https://linkedin.com/in/JacklynBiggin"><FontAwesomeIcon width="0" icon={faLinkedin} /></a>
+                <a href="https://github.com/JacklynBiggin"><FontAwesomeIcon width="0" icon={faGithub} /></a>
                 <a href="https://twitter.com/JackBiggin"><FontAwesomeIcon width="0" icon={faTwitter} /></a>
                 <a href="https://youtube.com/@JacklynBiggin"><FontAwesomeIcon width="0" icon={faYoutube} /></a>
-                <a href="https://twitch.tv/JacklynBiggin"><FontAwesomeIcon width="0" icon={faTwitch} /></a>
+                {/*<a href="https://twitch.tv/JacklynBiggin"><FontAwesomeIcon width="0" icon={faTwitch} /></a>*/}
                 <br className="d-none d-md-inline" />
                 <a className={styles.tag} href="./Jacklyn%20Biggin%20Resume.pdf"><div>Resume</div></a>
+                <a className={styles.tag} href="mailto:hi@jacklyn.dev"><div>Email Me</div></a>
             </div>
         </div>
     )
